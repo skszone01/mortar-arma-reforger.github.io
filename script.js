be91cad7bcffad12e06088d46287bdcc7fc5a1dc
skszone01 <50658778+skszone01@users.noreply.github.com>
2025-09-28 16:07:59 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2015,3 +2015,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Then initialize the calculator and store reference
     window.mortarCalculator = new MortarCalculator();
 });
+
+/*
+ * =======================================================================
+ * ไม่อนุญาตให้นำไปใช้เพื่อแสวงหาผลกำไรใดๆทั้งสิ้น
+ * หากนำไปใช้งานในส่วนอื่น ใส่เครดิต Youtube : Sitt Chanel ให้ด้วย
+ * 
+ * Not allowed to be used for any commercial purposes.
+ * If used in other projects, please credit Youtube : Sitt Chanel
+ * =======================================================================
+ */
